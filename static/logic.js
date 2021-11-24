2ee7e71bd56c60e68b0945469bc61df57de564e6
Albert Dominguez <albert.j.dominguez21@gmail.com>
2021-11-23 21:48:20 -0600

    deleted moralis functions

--- static/logic.js
+++ static/logic.js
@@ -21,29 +21,3 @@ async function login(){
 function formatWalletAddress(account){
     return (account.substring(0, 5)) + "......." + (account.slice(-5));
 }
-function deactivateControls(){
-    document.getElementById("connect-to-wallet").style.display = "none";
-}
-async function populate(){
-    const balanaces = await Moralis.Web3API.account.getTokenBalances({chain: chainToQuery}).then(buildTableBalances);
-}
-function buildTableBalances(){
-    document.getElementById("resultBalances").innerHTML = `<table class="table table-dark table-striped" id="balancesTable">
-                                                                    </table>`;
-    const table = document.getElementById("balancesTable");
-    const rowHeader = `<thead>
-                            <tr>
-                                <th>Token</th>
-                                <th>Symbol</th>
-                                <th>Balance</th>
-                            </tr>
-                        </thead>`;
-    for(let i = 0; i < data.length; i++){
-        let row = `<tr>
-                        <td>${data[i].name}</td>
-                        <td>${data[i].symbol}</td>
-                        <td>${data[i].balance/10**18}</td>
-                    </tr>`
-        table.innerHTML += row;
-    }
-}
