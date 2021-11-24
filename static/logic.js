0ea619bfefa3c9b251ad4d58b5a32d0ba26f7c39
Albert Dominguez <albert.j.dominguez21@gmail.com>
2021-11-23 23:14:04 -0600

    cleaned up

--- static/logic.js
+++ static/logic.js
@@ -4,6 +4,7 @@ if (typeof window.ethereum !== 'undefined') {
 } else {
     console.log('MetaMask is not installed, please install and try again.')
 }
+//Instantiate Web3.js
 let web3;
 try {
     web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
@@ -12,24 +13,24 @@ try {
     console.log("Could not instantiate Web3.js");
 }
 async function login(){
+    //Will prompt user to login via metamask
     const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    //When user successfully logs in via Metamask
     const account = accounts[0];
     document.getElementById("connect-to-wallet").style.display = "none";
     document.getElementById("wallet-address-div").style.display = "block";
     document.getElementById("wallet-address-top").innerText = (formatWalletAddress(account));
     var chainId = await getChain();
-    // console.log(chainId)
     var balance = await web3.eth.getBalance(account).then(bal=> {
         balance = web3.utils.fromWei(bal, "ether");
         document.getElementById("balance").style.display = "block";
         document.getElementById("resultBalances").innerText = `${balance} ${chainId}`;
     });
-
-    //ETH chain id = 1
-    //BSC chain id = 56
 }
 async function getChain(){
     return await web3.eth.net.getId().then(chainID=>{
+        //ETH chain id = 1
+        //BSC chain id = 56
         if (chainID === 1){
             return "ETH";
         }
