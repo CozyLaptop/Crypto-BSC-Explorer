2d268a23513b34f376a2f7d892ee1f0f358082c3
Albert Dominguez <albert.j.dominguez21@gmail.com>
2021-11-28 04:02:11 -0600

    will convert your crypto balance into USD

--- static/logic.js
+++ static/logic.js
@@ -12,6 +12,7 @@ try {
 }catch (e) {
     console.log("Could not instantiate Web3.js");
 }
+var coinGeckoID;
 //On click of connect MetaMask button
 async function login(){
     //Will prompt user to login via metamask
@@ -21,27 +22,42 @@ async function login(){
     document.getElementById("connect-to-wallet").style.display = "none";
     document.getElementById("wallet-address-div").style.display = "block";
     document.getElementById("wallet-address-top").innerText = (formatWalletAddress(account));
-    var chainId = await getChain();
-    var balance = await web3.eth.getBalance(account).then(bal=> {
-        balance = web3.utils.fromWei(bal, "ether");
-        document.getElementById("balance").style.display = "block";
-        document.getElementById("resultBalances").innerText = `${balance} ${chainId}`;
+    var chainId = await web3.eth.net.getId();
+    var chainString = getChain(chainId);
+    var balanceInCrypto = await web3.eth.getBalance(account).then(bal=> {
+        balanceInCrypto = web3.utils.fromWei(bal, "ether");
+    }).then(()=>{
+        document.getElementById("resultBalances").innerText = `${balanceInCrypto} ${chainString}`;
+    }).then(()=>{
+        getBalanceInUSD(balanceInCrypto);
     });
+    document.getElementById("balance").style.display = "block";
 }
-async function getChain(){
-    return await web3.eth.net.getId().then(chainID=>{
-        //ETH chain id = 1
-        //BSC chain id = 56
-        //MATIC chain id = 137
-        if (chainID === 1){
-            return "ETH";
-        }
-        if (chainID === 56){
-            return "BNB";
-        }
-        if (chainID === 137){
-            return "MATIC";
-        }
+function getChain(chainID){
+    //ETH chain id = 1
+    //BSC chain id = 56
+    //MATIC chain id = 137
+    if (chainID === 1){
+        coinGeckoID = "ethereum";
+        return "ETH";
+    }
+    if (chainID === 56){
+        coinGeckoID = "binancecoin";
+        return "BNB";
+    }
+    if (chainID === 137){
+        coinGeckoID = "matic-network";
+        return "MATIC";
+    }
+}
+
+async function getBalanceInUSD(balanceInCrypto) {
+    await fetch("https://api.coingecko.com/api/v3/" + "/coins/" + coinGeckoID).then(response => {
+        response.json().then(coin => {
+            var currentPrice = parseFloat(coin.market_data.current_price.usd);
+            parseFloat(balanceInCrypto)
+            document.getElementById("balanceInUSD").innerText = `$${(currentPrice * balanceInCrypto).toFixed(2)}`;
+        });
     });
 }
 function formatWalletAddress(account){
